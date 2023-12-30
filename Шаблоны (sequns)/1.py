e3033eac1b76e0bfbc515c6a74290bb87bc8ba6a
rick1 <rick11@yandex.ru>
2023-12-30 17:33:55 +0300

    Первый коммит

--- Шаблоны (sequns)/1.py
+++ Шаблоны (sequns)/1.py
@@ -1,21 +1,21 @@
-# Нахождение символов, которые повторяются
+#  Нахождение символов, которые повторяются
 string = "Hello world"
 dublic = set([el for el in string if string.count(el) > 1])
 print(dublic)
 
-# 2 Можно и строки и список
+#  2 Можно и строки и список
 word_x, word = list("газета"), "арбуз"
 print(set(word).intersection(word_x))
 
-# Метод replace в списочном выражении
+#  Метод replace в списочном выражении
 str_list = ["кот", "собака", "кот собака", "кот кот"]
 result_list = [elem.replace("кот", "кошка", 1) for elem in str_list]
 print(result_list)  # Выведет ['кошка', 'собака', 'кошка собака', 'кошка кот']
 
-# удаление во множестве небуквенных символов
+#  удаление во множестве небуквенных символов
 set_m = set('asd,g?ty.qwety!')
 print(set_m.difference("'!?:-—/.,'"))
 
-# Цвет текста в консоли
+#  Цвет текста в консоли
 print("\033[36m asdfg")  # бирюзовый
 print("\033[31m asdfg")  # красный
